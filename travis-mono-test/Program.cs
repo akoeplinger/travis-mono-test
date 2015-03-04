8c3f443f287d30c01eb2faaa25adcf337bc690c6
Alexander Köplinger <alex.koeplinger@outlook.com>
2015-03-04 01:05:49 +0100

    WIP

---
+++ travis-mono-test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace travis_mono_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test_library.Class1.DoIt();
+        }
+    }
+}
