e0c1ffc45d6cfc80be122e80897818e5747ba0fb
Alexander Köplinger <alex.koeplinger@outlook.com>
2015-03-28 21:17:33 +0100

    Update Program.cs

--- travis-mono-test/Program.cs
+++ travis-mono-test/Program.cs
@@ -8,7 +8,7 @@ namespace travis_mono_test
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)asd
         {
             test_library.Class1.DoIt();
         }
