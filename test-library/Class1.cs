c23b00921f823a03cf2c0eb5b2d0cbc0cc5b3d49
Alexander Köplinger <alex.koeplinger@outlook.com>
2015-04-10 12:43:48 +0200

    Update Class1.cs

--- test-library/Class1.cs
+++ test-library/Class1.cs
@@ -12,6 +12,7 @@ namespace test_library
         public static void DoIt() 
         {
             Console.WriteLine("Hello World: " + DateTime.UtcNow.ToString("D") + " :-)");
+            Console.WriteLine("EnvVar: " + Environment.GetEnvironmentVariable("MyVar"));
 			Type type = Type.GetType("Mono.Runtime");
 if (type != null)
 {
