65b2ecf14f27fe7dcb16c06e094849ee7aa81df1
Alexander Köplinger <alex.koeplinger@outlook.com>
2015-07-28 12:35:04 +0200

    Add OSX and weekly + reformat

--- test-library/Class1.cs
+++ test-library/Class1.cs
@@ -12,14 +12,14 @@ namespace test_library
         public static void DoIt() 
         {
             Console.WriteLine("Hello World: " + DateTime.UtcNow.ToString("D") + " :-)");
-            Console.WriteLine("EnvVar: " + Environment.GetEnvironmentVariable("MyVar", EnvironmentVariableTarget.Machine));
-			Type type = Type.GetType("Mono.Runtime");
-if (type != null)
-{
-    MethodInfo displayName = type.GetMethod("GetDisplayName", BindingFlags.NonPublic | BindingFlags.Static);
-    if (displayName != null)
-        Console.WriteLine(displayName.Invoke(null, null));
-}
+            Console.WriteLine("EnvVar: " + Environment.GetEnvironmentVariable("TRAVIS_REPO_SLUG", EnvironmentVariableTarget.Machine));
+            Type type = Type.GetType("Mono.Runtime");
+            if (type != null)
+            {
+                MethodInfo displayName = type.GetMethod("GetDisplayName", BindingFlags.NonPublic | BindingFlags.Static);
+                if (displayName != null)
+                    Console.WriteLine(displayName.Invoke(null, null));
+            }
         }
     }
 }
